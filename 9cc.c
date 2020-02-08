5387258195f096b07adbfa68daa8665929be6155
kikki-git <kikkidev64@gmail.com>
2020-02-08 14:13:00 +0000

    単項プラス・マイナスを追加

--- 9cc.c
+++ 9cc.c
@@ -56,6 +56,7 @@ Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
 Node *new_node_num(int val);
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 void gen(Node *node);
 
@@ -90,16 +91,23 @@ Node *expr() {
 }
 
 Node *mul(){
-    Node *node = primary();
-
+    Node *node = unary();
     for(;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+Node *unary(){
+    if (consume('+'))
+        return primary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), primary());
+    return primary();
 }
 
 Node *primary() {
